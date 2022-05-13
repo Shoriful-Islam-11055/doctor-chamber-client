7246d460018e001a94e5d738ebfe2613b1c4b19b
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-05-13 23:32:37 +0600

    navbar and link setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element = {<Home></Home>}></Route>
+        <Route path='/about' element = {<About></About>}></Route>
+      </Routes>
     </div>
   );
 }

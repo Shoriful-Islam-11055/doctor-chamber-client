4db9ad61d41042502b8e91a1f1e987a1d25976dd
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-05-15 23:31:27 +0600

    Testimonial card and components add

--- src/Pages/Home/Testimonial.js
+++ src/Pages/Home/Testimonial.js
@@ -1,20 +1,58 @@
-import React from 'react';
-import quote from '../../assets/icons/quote.svg'
+import React from "react";
+import quote from "../../assets/icons/quote.svg";
+import TestimonialCard from "./TestimonialCard";
+import people1 from "../../assets/images/people1.png";
+import people2 from "../../assets/images/people2.png";
+import people3 from "../../assets/images/people3.png";
 
 const Testimonial = () => {
-    return (
-        <div className='mx-6'>
-            <div className='flex justify-between'>
-                <div className='mt-[50px]'>
-                    <h3>Testimonial</h3>
-                    <h2>What Our Patients Says</h2>
-                </div>
-                <div>
-                    <img className='max-w-[192px] h-[156px]' src={quote} alt="" />
-                </div>
-            </div>
+  const Patients = [
+    {
+      _id: 1,
+      name: "Jony",
+      description:
+        "It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content",
+      city: "Dhaka",
+      img: people1,
+    },
+    {
+      _id: 2,
+      name: "Rocy",
+      description:
+        "It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content",
+      city: "Dhaka",
+      img: people2,
+    },
+    {
+      _id: 3,
+      name: "Jacy",
+      description:
+        "It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content",
+      city: "Dhaka",
+      img: people3,
+    },
+  ];
+  return (
+    <div className="mx-6">
+      <div className="flex justify-between">
+        <div className="mt-[50px]">
+          <h3 className="text-primary text-3xl">Testimonial</h3>
+          <h2 className="text-4xl">What Our Patients Says</h2>
         </div>
-    );
+        <div>
+          <img className="max-w-[192px] h-[156px]" src={quote} alt="" />
+        </div>
+      </div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-col-2 gap-6">
+        {Patients.map((patient) => (
+          <TestimonialCard
+            patient={patient}
+            key={patient._id}
+          ></TestimonialCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Testimonial;
---
+++ src/Pages/Home/TestimonialCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TestimonialCard = ({patient}) => {
+  return (
+    <div className="mt-12">
+      <div class="card bg-base-100 shadow-2xl">
+        <div class="card-body text-black">
+          <p>{patient.description}</p>
+          <div class="flex items-center">
+            <div><img className="mr-8" src={patient.img} alt="" /></div>
+            <div>
+                <p className="text-2xl text-bold">{patient.name}</p>
+                <p className="text-xl">{patient.city}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;

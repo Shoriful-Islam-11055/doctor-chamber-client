2103d1dfdc7aa4fa372b9a9c5b049e1e84b6abc0
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-05-20 10:11:20 +0600

    login form validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,21 +1,29 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import auth from "../../firebase.init";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 const Login = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
   if (user) {
     console.log(user.firstName);
   }
+
+  const onSubmit = (data) => console.log(data);
 
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="card w-96 bg-base-100 shadow-2xl p-6">
         <div className="">
           <h2 className="text-center text-4xl font-bold py-6">LOGIN</h2>
-          <form>
-              
+
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control w-full max-w-full">
               <label className="label">
                 <span className="text-2xl">Email</span>
@@ -23,8 +31,30 @@ const Login = () => {
               <input
                 type="email"
                 placeholder="Your email"
-                className="input input-bordered w-full max-w-full mb-2 text-xl"
+                className="input input-bordered w-full max-w-full text-xl"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required",
+                  },
+                  pattern: {
+                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                    message: "Email is not valid", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
               />
+              <label className="label">
+                {errors.email?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
             </div>
 
             <div className="form-control w-full max-w-full">
@@ -33,13 +63,30 @@ const Login = () => {
               </label>
               <input
                 type="password"
-                placeholder="Password"
-                className="input input-bordered w-full max-w-full text-xl"
+                placeholder="Your password"
+                className="input input-bordered w-full max-w-full  text-xl"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "password is required",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Length must be 6 or more", // JS only: <p>error message</p> TS only support string
+                  },
+                })}
               />
-              <label className="label mb-2">
-                <span className="label-text-alt cursor-pointer text-lg text-red-500">
-                  Forgot password
-                </span>
+              <label className="label">
+                {errors.password?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
               </label>
             </div>
 
@@ -48,7 +95,7 @@ const Login = () => {
                 name="submit"
                 type="submit"
                 value="SUBMIT"
-                className="btn btn-wide w-full"
+                className="btn btn-wide w-full mt-3"
               />
               <label className="text-center">
                 New to chamber?&nbsp;

bb58c716fa146d11a59713614cddc15074fec5c9
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-05-21 17:55:34 +0600

    add dashboard table and set data in table in dynamically

--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const MyAppointment = () => {
-    return (
-        <div>
-            <h1>My Appointment</h1>
-        </div>
-    );
+  const [appointments, setAppointments] = useState([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/booking?patient=${user.email}`)
+        .then((res) => res.json())
+        .then((data) => setAppointments(data));
+    }
+  }, [user]);
+  let p = 1;
+  return (
+    <div>
+      <h1 className="text-2xl">My Appointment {appointments.length}</h1>
+      <div class="overflow-x-auto px-6 mt-6">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Treatment</th>
+            </tr>
+          </thead>
+          <tbody>
+              {
+                  appointments.map( a =>  <tr>
+                    <th>{p++}</th>
+                    <td>{a.patientName}</td>
+                    <td>{a.date}</td>
+                    <td>{a.slot}</td>
+                    <td>{a.treatment}</td>
+                  </tr>
+                )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointment;
